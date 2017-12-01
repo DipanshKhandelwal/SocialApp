b8382b109dcf4dec0f42696430786fbac5943003
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-12-01 10:32:51 +0530

    Initialized project

---
+++ app/src/main/java/com/example/dipansh/socialapp/MainActivity.java
@@ -0,0 +1,13 @@
+gpackage com.example.dipansh.socialapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
